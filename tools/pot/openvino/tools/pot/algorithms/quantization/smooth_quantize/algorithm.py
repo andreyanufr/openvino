16a21afbe992cbf4bd87d5d7c00f8fe90fa3c221
andreyanufr <andrey.anufriev@intel.com>
2023-02-10 17:42:12 +0100

    modified SQ

--- tools/pot/openvino/tools/pot/algorithms/quantization/smooth_quantize/algorithm.py
+++ tools/pot/openvino/tools/pot/algorithms/quantization/smooth_quantize/algorithm.py
@@ -2,8 +2,11 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from copy import deepcopy
-
+import os
+import sys
 import numpy as np
+
+np.set_printoptions(threshold=sys.maxsize)
 
 from ...algorithm import Algorithm
 from ...algorithm_selector import COMPRESSION_ALGORITHMS
@@ -40,6 +43,10 @@ class SmoothQuantize(Algorithm):
         self._sampler = create_sampler(
             engine, stat_subset_size, shuffle_data, seed, stat_batch_size)
         self.alpha = 0.5
+        self.history = {}
+
+        self.layer_number = self._config.get('layer_number', -1)
+        print("self.layer_number ", self.layer_number)
 
     @property
     def change_original_model(self):
@@ -60,13 +67,39 @@ class SmoothQuantize(Algorithm):
             for stats_name, stats_values in stats_list.items():
                 stats[node_name][stats_name] = agf.median(stats_values)
 
+        print("Process {} MatMul nodes".format(len(mat_mul_inputs)))
+
+        if 1:
+            mat_mul_inputs_groupped = self.group_nodes_by_source(mat_mul_inputs)
+            num_layer = 0
+            for k, val in mat_mul_inputs_groupped.items():
+                if 'Transpose_' in k.fullname or 'Softmax_' in k.fullname:
+                    continue
+                if num_layer == self.layer_number or True:
+                    self.smooth_quantize_groupped(k, val, stats)
+                num_layer += 1
+            return model
+
         for node_mat_mul_data in mat_mul_inputs:
             node_0, node_1, node_mat_mul = node_mat_mul_data
-            self.smooth_quantize(model, node_0, node_1, node_mat_mul, stats)
+            self.smooth_quantize(node_0, node_1, node_mat_mul, stats)
 
         return model
 
-    def smooth_quantize(self, model, node_0, node_1, node_mat_mul, stats):
+    @staticmethod
+    def group_nodes_by_source(mat_mul_inputs):
+        res = {}
+        for node_mat_mul_data in mat_mul_inputs:
+            node_0, node_1, node_mat_mul = node_mat_mul_data
+
+            if node_0 in res:
+                res[node_0].append((node_1, node_mat_mul))
+            else:
+                res[node_0] = [(node_1, node_mat_mul)]
+
+        return res
+
+    def smooth_quantize(self, node_0, node_1, node_mat_mul, stats):
         # node_0 * node_1 = node_mat_mul_out
         name_0 = node_0.fullname
         name_1 = node_1.fullname
@@ -92,6 +125,8 @@ class SmoothQuantize(Algorithm):
             # TODO: check it
             # if node_mat_mul['transpose_b']:
             #     stats_1 = np.transpose(stats_1) # copy
+            if not node_mat_mul['transpose_b']:
+                raise Exception("Bad transpose")
             stats_1 = np.abs(stats_1)
             stats_1 = np.max(stats_1, axis=0)  # abs_max per column [M, K] * [K, N]
             is_bmm = False
@@ -99,14 +134,181 @@ class SmoothQuantize(Algorithm):
         if is_bmm:
             self.smooth_quantize_activation_activation(node_0, node_1, node_mat_mul, stats_0, stats_1)
         else:
-            self.smooth_quantize_activation_linear(model, node_0, node_1, node_mat_mul, stats_0, stats_1)
+            self.smooth_quantize_activation_linear(node_0, node_1, node_mat_mul, stats_0, stats_1)
+
+    def smooth_quantize_groupped(self, node_in, dst_nodes, stats):
+        # node_0 * node_1 = node_mat_mul_out
+        name_in = node_in.fullname
+        # if name_in != 'Tanh_1241':
+        #     return
+        bmms = 0
+
+        if not name_in in stats:
+            raise Exception("Input node 0 is Const")
+
+        for dst_node in dst_nodes:
+            node_in_1, node_mat_mul = dst_node
+            name_in_1 = node_in_1.fullname
+            if name_in_1 in stats or node_in_1.type != 'Const':
+                bmms += 1
+
+        if bmms > 0:
+            print(f"Node {name_in} contains output for BMM")
+            return
+
+        best_scale = None
+        best_ratio = 0.0
+        stats_activation = stats[name_in]['channel_range_max']
+        np.save(os.path.join('stats', name_in.replace('/', '_') + '.npy'), stats_activation)
+        stats_activation = np.max(stats_activation, axis=0)
+        np.save(os.path.join('stats', name_in.replace('/', '_') + '_max.npy'), stats_activation)
+
+        print(f"Node {name_in} smooth quantize")
+        for dst_node in dst_nodes:
+            node_in_1, node_mat_mul = dst_node
+
+            if node_in_1.type != 'Const':
+                raise Exception("Wrong type of FC node")
+
+            stats_w = deepcopy(nu.get_node_value(node_in_1))
+
+            name_in_1 = node_in_1.fullname
+            np.save(os.path.join('stats', name_in.replace('/', '_') + "__" + name_in_1.replace('/', '_') + '_w.npy'),
+                    stats_w)
+
+            if not node_mat_mul['transpose_b']:
+                raise Exception("Bad transpose")
+            stats_w = np.abs(stats_w)
+            stats_w = np.max(stats_w, axis=0)  # abs_max per column [M, K] * [K, N]
+
+            np.save(
+                os.path.join('stats', name_in.replace('/', '_') + "__" + name_in_1.replace('/', '_') + '_stats_w.npy'),
+                stats_w)
+
+            scales = (np.power(stats_activation, self.alpha) / (
+                        np.power(stats_w, 1 - self.alpha) + np.finfo(float).eps))
+            # a_min = np.quantile(scales, 0.4)
+            a_min = np.quantile(scales, 0.8)
+            # a_min = 1e-5
+            scales = np.clip(scales, a_min=a_min, a_max=None)
+
+            np.save(
+                os.path.join('stats', name_in.replace('/', '_') + "__" + name_in_1.replace('/', '_') + '_scale.npy'),
+                scales)
+
+            ratio = scales.min() / scales.max()
+
+            if ratio > best_ratio:
+                best_ratio = ratio
+                best_scale = deepcopy(scales)
+
+        scales = best_scale ** (-1)
+        idxs = np.where(stats_activation > 0.0)
+        try:
+            non_zero_activations = stats_activation[idxs]
+        except:
+            non_zero_activations = stats_activation
+        ratio_before = non_zero_activations.min() / non_zero_activations.max()
+        stats_activation_s = stats_activation * scales
+        idxs = np.where(stats_activation_s > 0.0)
+        try:
+            non_zero_activations = stats_activation_s[idxs]
+        except:
+            non_zero_activations = stats_activation_s
+        ratio_after = non_zero_activations.min() / non_zero_activations.max()
+
+        if ratio_after <= ratio_before:
+            print("Skip activation statistics smoothing for ", node_in.id)
+            return
+
+        w_scales = np.expand_dims(best_scale, axis=0)
+
+        for dst_node in dst_nodes:
+            node_in_1, node_mat_mul = dst_node
+
+            weights = nu.get_node_value(node_in_1)
+            stats_w = np.abs(weights)
+            stats_w_max = np.max(stats_w, axis=1)
+            stats_w_min = np.min(stats_w, axis=1)
+            ratio = stats_w_max / (stats_w_min + 0.0000000001)
+            print("Ratio before: ", ratio.min(), ratio.max())
+
+            weights = weights * w_scales
+
+            stats_w = np.abs(weights)
+            stats_w_max = np.max(stats_w, axis=1)
+            stats_w_min = np.min(stats_w, axis=1)
+            ratio = stats_w_max / (stats_w_min + 0.0000000001)
+            print("Ratio after: ", ratio.min(), ratio.max())
+
+            nu.set_node_value(node_in_1, weights)
+
+            name_in_1 = node_in_1.fullname
+            np.save(
+                os.path.join('stats', name_in.replace('/', '_') + "__" + name_in_1.replace('/', '_') + '_w_scaled.npy'),
+                weights)
+
+        np.save(os.path.join('stats', name_in.replace('/', '_') + '_best_scale.npy'), best_scale)
+
+        scales = best_scale ** (-1)
+        idxs = np.where(stats_activation > 0.0)
+        try:
+            non_zero_activations = stats_activation[idxs]
+        except:
+            print("Strange shape: ", stats_activation.shape)
+            non_zero_activations = stats_activation
+
+        print("Activation ratio before: ", non_zero_activations.min() / non_zero_activations.max())
+        stats_activation = stats_activation * scales
+        idxs = np.where(stats_activation > 0.0)
+        try:
+            non_zero_activations = stats_activation[idxs]
+        except:
+            print("Strange shape: ", stats_activation.shape)
+            non_zero_activations = stats_activation
+        print("Activation ratio after: ", non_zero_activations.min() / non_zero_activations.max())
+
+        scales = np.expand_dims(scales, axis=(0, 1))
+
+        destination_ports = []
+        for dest_port in node_in.out_port(0).get_destinations():
+            destination_ports.append(dest_port)
+
+        node_name = node_in.id + '/sq_mul'
+        mul_node = Mul(node_in.graph, {'name': node_name, 'need_shape_inference': True}).create_node()
+        mul_node['fullname'] = node_name
+        mul_node['any_name'] = node_name
+        mul_node['id'] = node_name
+        mul_node['name'] = node_name
+
+        const_node = Const(node_in.graph, {'name': node_in.id + '/sq', 'value': mo_array(scales)}).create_node()
+        const_node['fullname'] = node_in.id + '/sq'
+        const_node.out_port(0).connect(mul_node.in_port(1))
+
+        node_in.out_port(0).disconnect()
+        node_in.out_port(0).connect(mul_node.in_port(0))
+        # node_in.out_port(0).get_connection().add_destination(mul_node.in_port(0))
+
+        connected_ports = []
+        for dst_node in dst_nodes:
+            node_in_1, node_mat_mul = dst_node
+            mul_node.out_port(0).connect(node_mat_mul.in_port(0))
+            # mul_node.out_port(0).get_connection().add_destination(node_mat_mul.in_port(0))
+
+            connected_ports.append(node_mat_mul.in_port(0))
+
+        for dest_port in destination_ports:
+            if dest_port in connected_ports:
+                continue
+            node_in.out_port(0).connect(dest_port)
 
     def smooth_quantize_activation_activation(self, node_0, node_1, node_mat_mul, stats_0, stats_1):
         # node_0 * node_1 = node_mat_mul_out
         # TODO: theoretical place for improvement
-        pass
+        print("MatMul {} is BMM.".format(node_mat_mul.name))
+        return 0
 
-    def smooth_quantize_activation_linear(self, model, node_a, node_l, node_mat_mul, stats_a, stats_l, ratio=0.5):
+    def smooth_quantize_activation_linear(self, node_a, node_l, node_mat_mul, stats_a, stats_l, ratio=0.5):
         # node_0 * node_1 = node_mat_mul_out, [M, K] * [K, N] = [M, N]
 
         # check it later
@@ -121,6 +323,8 @@ class SmoothQuantize(Algorithm):
         #     # no reason to make smoothing
         #     return
 
+        # if node_a.id in self.history:
+        #     return
         scales = (np.power(stats_a, self.alpha) / (np.power(stats_l, 1 - self.alpha) + np.finfo(float).eps))
         scales = np.clip(scales, a_min=1e-5, a_max=None)
 
@@ -133,11 +337,8 @@ class SmoothQuantize(Algorithm):
         # scales = np.broadcast_to(scales, stats_a)
         scales = scales ** (-1)
 
+        # self.history[node_a.id] = deepcopy(w_scales)
         self.insert_multiply_node(node_a, node_mat_mul, scales)
-
-        # TODO: need to insert Multiply node between node_a and node_mat_mul with scale**(-1)
-
-        pass
 
     @staticmethod
     def insert_multiply_node(node_in, node_out, scales):
@@ -164,9 +365,27 @@ class SmoothQuantize(Algorithm):
 
         # ##node_out.graph.remove_edge(node_out.in_port(0).id, node_in.out_port(0).id)
 
-        mul_node = Mul(node_out.graph, {'name': node_out.id + '/sq_mul', 'need_shape_inference': True}).create_node()
-        const_node = Const(node_out.graph, {'name': node_out.id + '/sq', 'value': mo_array(scales)}).create_node()
+        # mul_node = Mul(node_out.graph, {'name': node_out.id + '/sq_mul', 'need_shape_inference': True}).create_node()
+        # mul_node['fullname'] = node_out.id + '/sq_mul'
+        # mul_node['any_name'] = node_out.id + '/sq_mul'
+
+        node_name = node_in.id + '/sq_mul' + node_out.id
+        mul_node = Mul(node_out.graph, {'name': node_name, 'need_shape_inference': True}).create_node()
+        mul_node['fullname'] = node_name
+        mul_node['any_name'] = node_name
+        mul_node['id'] = node_name
+        mul_node['name'] = node_name
+
+        # const_node = Const(node_out.graph, {'name': node_out.id + '/sq', 'value': mo_array(scales)}).create_node()
+        # const_node['fullname'] = node_out.id + '/sq'
+        # const_node.out_port(0).connect(mul_node.in_port(1))
+
+        const_node = Const(node_out.graph, {'name': node_in.id + '/sq', 'value': mo_array(scales)}).create_node()
+        const_node['fullname'] = node_in.id + '/sq'
         const_node.out_port(0).connect(mul_node.in_port(1))
+
+        # mul_node.out_port(0)['any_name'] = node_name + "/out"
+        # mul_node.out_port(0)['name'] = node_name + "/out"
 
         mul_node.out_port(0).connect(node_out.in_port(0))
         mul_node.out_port(0).get_connection().set_destination(node_out.in_port(0))
